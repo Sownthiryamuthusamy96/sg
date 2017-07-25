5aa24c3f99b953654d75119232684ece052bc8c2
Sownthiryamuthusamy96 <sownthiryamuthusamy@gmail.com>
2017-07-25 17:11:31 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+pubic static void main(String[] args)
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+Double n=Double.parseDouble(br.readLine());
+Double n1=Double.parseDouble(br.readLine());
+Double c=Math.pow(n,n1);
+System.out.println(c);
+}
+}
